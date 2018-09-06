82f432dbd0c91bb6b91826c2a0978fb87170c1a4
Rile14929 <rile14929@163.com>
2018-09-06 13:04:25 +0800

    按钮增加左右icon

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,11 @@
 <template>
-  <button class="g-button" :class="{['iconPosition']:true}">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
     <svg v-if="icon" class="icon">
       <use :xlink:href="`#i-${icon}`"></use>
     </svg>
-    <slot></slot>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 <script>
@@ -22,19 +24,21 @@ export default {
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  vertical-align: middle;
   &:hover{
-  border-color: var(--border-color-hover)
-}
+    border-color: var(--border-color-hover)
+  }
   &:active{
     border-color: var(--border-active-bg)
   }
   &:focus {
     outline: none;
   }
+  > .icon{ order:1}
+  > .content{ order:2;margin-left: .1em}
   &.icon-right{
-    > .icon{
-      order: 2
-    }
+    > .icon{ order: 2;margin-left:.1em;margin-right: 0}
+    > .content{ order:1;}
   }
 }
 </style>
