9c9fe94d8c644863672dae362e0ad25c2f4c4989
Rile14929 <rile14929@163.com>
2018-09-09 00:36:28 +0800

    使用chai 添加测试用例

--- src/app.js
+++ src/app.js
@@ -21,11 +21,60 @@ const expect = chai.expect
   const Constructor = Vue.extend(Button)
   const button = new Constructor({
     propsData: {
-      icon: 'settings'
+      icon: 'setting'
     }
   })
   button.$mount('#test')
   let useElement = button.$el.querySelector('use')
-  console.log(useElement)
-  expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-setting')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon:'setting',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon:'setting',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon:'setting'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function(){
+    expect(1).to.eq(1)
+    console.log(1)
+  })
+  let button = vm.$el
+  button.click()
 }
